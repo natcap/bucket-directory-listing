7fbffc5855d47d0f80293e1239cef6add35de9d3
David Fisher <davemfish@gmail.com>
2019-05-21 13:12:34 -0700

    renaming some variables.

--- latest.js
+++ latest.js
@@ -1,27 +1,32 @@
-function readLatestInstallers(latest_lookup) {
+function readLatestInstallers(redirect_lookup) {
   // load json object that stores URLs for the latest installers
-  let latest = fetch(latest_lookup)
+  let redirections = fetch(redirect_lookup)
     .then(function(response){
       if (response.status === 200){
         return response.json()
       } else {
-        console.warn(response.status);
+        console.log(response.status);
       }
     })
-  return latest;
+  return redirections;
 }
 
-async function locationHashLatest() {
+async function locationHashRedirect(redirect_lookup_json) {
   // trigger redirect to download a file given a special hash URL
   // if the URL doesn't match, load directory listing as normal.
-  let latest = await readLatestInstallers(CONFIG.special_bucket_objects)
-  if (typeof latest !== 'undefined') {
-    if (Object.keys(latest).includes(location.hash)) {
-      window.location.href = latest[location.hash]
+  let redirections = await readLatestInstallers(redirect_lookup_json)
+  if (typeof redirections !== 'undefined') {
+    if (Object.keys(redirections).includes(location.hash)) {
+      window.location.href = redirections[location.hash]
     }
   }
 }
 
 import { listing_config } from './config.js';
 var CONFIG = listing_config[location.host]
-window.onhashchange = locationHashLatest()
+window.onhashchange = locationHashRedirect(CONFIG.special_bucket_objects)
+
+
+// test
+// given a json file and a url string with a hash
+// expect that window.location.href goes to the correct bucket file.
