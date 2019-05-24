2a0ade52f504414b43792ef56a53919a1670738b
David Fisher <davemfish@gmail.com>
2019-05-24 15:37:45 -0700

    removed the frag id lookup json from this repo.

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ var listing_config = {
 		exclude_files: ["index.html", "bucketlist.js", "latest.js", "latest.json", "config.js", "readme.md" ],
 		// sort_option: "A2Z", // 'A2Z', 'Z2A', 'BIG2SMALL', 'SMALL2BIG'
 		// root_dir: "invest/",
-		special_bucket_objects: "latest.json" // (optional) create special URLs that redirect to objects: {"#latest-invest-windows": "http://releases.naturalcapitalproject.org/invest/3.7.0/InVEST_3.7.0_x86_Setup.exe"}
+		special_bucket_objects: "fragment_id_redirections.json" // (optional) create special URLs that redirect to objects: {"#latest-invest-windows": "http://releases.naturalcapitalproject.org/invest/3.7.0/InVEST_3.7.0_x86_Setup.exe"}
 	},
 	"localhost:8000": { // another bucket domain
 		bucket_url: "https://www.googleapis.com/storage/v1/b/releases.naturalcapitalproject.org/o",
 		exclude_files: ["index.html", "bucketlist.js", "latest.js", "latest.json", "config.js", "readme.md"],
 		// sort_option: "A2Z", // 'A2Z', 'Z2A', 'BIG2SMALL', 'SMALL2BIG'
 		// root_dir: "invest/"	,
-		special_bucket_objects: "latest.json"
+		special_bucket_objects: "fragment_id_redirections.json"
 	},
 }
 
