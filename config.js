b32425d950100f2b2d9925de166e52e01fc5eb71
davemfish <davemfish@gmail.com>
2020-12-04 06:15:17 -0800

    deprecate the fragment-id-for-static-links feature. fixes #2

--- config.js
+++ config.js
@@ -2,25 +2,23 @@ var listing_config = {
 	"releases.naturalcapitalproject.org": { // public domain of a bucket
 		bucket_url: "https://www.googleapis.com/storage/v1/b/releases.naturalcapitalproject.org/o",
 		public_url: "https://storage.googleapis.com/releases.naturalcapitalproject.org",
-		exclude_files: ["index.html", "bucketlist.js", "fragment_id_redirect.js", "fragment_id_redirections.json", "config.js", "readme.md" , "robots.txt"],
+		exclude_files: ["index.html", "bucketlist.js", "config.js", "readme.md" , "robots.txt"],
 		// If entries under a special prefix should be sorted in a special way
 		prefix_sort_map: {
 			"invest": "semver"
 		},
 		// sort_option: "A2Z", // 'A2Z', 'Z2A', 'BIG2SMALL', 'SMALL2BIG'
 		// root_dir: "invest/",
-		special_bucket_objects: "fragment_id_redirections.json" // (optional) create special URLs that redirect to objects: {"#latest-invest-windows": "http://releases.naturalcapitalproject.org/invest/3.7.0/InVEST_3.7.0_x86_Setup.exe"}
 	},
 	"localhost:8000": { // another bucket domain, this one convenient for local development.
 		bucket_url: "https://www.googleapis.com/storage/v1/b/releases.naturalcapitalproject.org/o",
 		public_url: "https://storage.googleapis.com/releases.naturalcapitalproject.org",
-		exclude_files: ["index.html", "bucketlist.js", "fragment_id_redirect.js", "fragment_id_redirections.json", "config.js", "readme.md", "robots.txt"],
+		exclude_files: ["index.html", "bucketlist.js", "config.js", "readme.md", "robots.txt"],
 		prefix_sort_map: {
 			"invest/": "semver"
 		},
 		// sort_option: "A2Z", // 'A2Z', 'Z2A', 'BIG2SMALL', 'SMALL2BIG'
 		// root_dir: "invest/"	,
-		special_bucket_objects: "fragment_id_redirections.json"
 	},
 }
 
--- readme.md
+++ readme.md
@@ -10,15 +10,3 @@ make sure `config.js` has a listing for the bucket where you wish to deploy
 ## Deploy: 
 `cd bucket-directory-listing`  
 `gsutil -m rsync . gs://releases.naturalcapitalproject.org`
-
-## Notes:  
-`fragment_id_redirect.js` is included in the deployed site by default. It enables static 'fragment identifiers' (e.g. #some-string) in URLs to point to special files(objects), whose names change frequently.  
-
-The identifiers and object names are specified in a file called fragment_id_redirections.json that should live in the bucket's root.
-```
-{
-  "#latest-invest-windows": "http://releases.naturalcapitalproject.org/invest/3.7.0/InVEST_3.7.0_x86_Setup.exe",
-  "#latest-invest-mac": "http://releases.naturalcapitalproject.org/invest/3.7.0/InVEST-3.7.0-mac.zip",
-  "#latest-invest-userguide": "http://releases.naturalcapitalproject.org/invest-userguide/latest/"
-}
-```
